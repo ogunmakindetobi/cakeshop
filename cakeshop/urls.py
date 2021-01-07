a752c6739e4174061f0265444283e74e7be2958f
ogunmakindetobi <ogunmakindetobi@outlook.com>
2021-01-07 12:15:30 +0000

    setup allauth

--- cakeshop/urls.py
+++ cakeshop/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
